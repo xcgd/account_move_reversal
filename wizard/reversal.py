62fb780fcb32eb33b10a037ab40182c7cf6bd5de
Anael Lorimier <anael.lorimier@xcg-consulting.fr>
2014-08-26 17:31:29 +0200

    Reconcile reversed moves

--- wizard/reversal.py
+++ wizard/reversal.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
+
 from openerp.osv import fields
 from openerp.osv import osv
 
@@ -12,12 +14,28 @@ class account_move_reversal_create(osv.osv_memory):
     _name = 'account.move.reversal.create'
 
     _columns = {
-        'period_id': fields.many2one('account.period', _('Period')),
+        'period_id': fields.many2one(
+            'account.period', _('Period'), required=True
+        ),
     }
 
     _defaults = {
         'period_offset': 0
     }
+
+    def reconcile_moves(self, cr, uid, moves, context):
+        aml_obj = self.pool['account.move.line']
+        
+        line_ids = defaultdict(list)
+
+        # We regroup amls by account
+        for move in moves:
+            for line in move.line_id:
+                line_ids[line.account_id.id].append(line.id)
+
+        # we reconcile for each account
+        for ids in line_ids.values():
+            aml_obj.reconcile(cr, uid, ids, context=context)
 
     def create_reversals(self, cr, uid, ids, context=None):
         wizard = self.browse(cr, uid, ids[0], context=context)
@@ -29,9 +47,13 @@ class account_move_reversal_create(osv.osv_memory):
             cr, uid, context['active_ids'], context=context
         ):
 
-            move_obj.reverse_move(
+            reversed_move_id = move_obj.reverse_move(
                 cr, uid, move.id, move.journal_id.id, period_id, context=context
             )
+            reversed_move = move_obj.browse(
+                cr, uid, [reversed_move_id], context
+            )[0]
+            self.reconcile_moves(cr, uid, [move, reversed_move], context)
 
         # Call post functions set by caller
         call_post_function(cr, uid, context)
